29934464755babf76ac660f24b56414f4c324ef3
Olesya <olesya-polishchuk@yandex.ru>
2017-04-08 18:23:03 +0300

    Initial commit

---
+++ Web/app/js/common.js
@@ -0,0 +1,24 @@
+var path = {
+    dist: { 
+        html: 'dist/',
+        js: 'dist/js/',
+        css: 'dist/css/',
+        img: 'dist/img/',
+        fonts: 'dist/fonts/'
+    },
+    app: { 
+        html: 'app/*.html',
+        js: 'app/js/common.js',
+        style: 'app/css/main.sass',
+        img: 'app/img/**/*.*',
+        fonts: 'app/fonts/**/*.*'
+    },
+    watch: { 
+        html: 'app/**/*.html',
+        js: 'app/js/**/*.js',
+        style: 'app/css/**/*.css',
+        img: 'app/img/**/*.*',
+        fonts: 'app/fonts/**/*.*'
+    },
+    clean: './dist
+};
